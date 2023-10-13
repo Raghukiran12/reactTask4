5684d60769707c850961ad3b51b6dd7e728ce9f6
Raghu Kiran <Raghu@raghukiran188@gmail.com>
2023-10-13 15:11:53 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import T4 from './Components_4/T4';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Dummy Data</h1>
+      </header>
+      <T4 />
+    </div>
+  );
+}
+
+
+export default App;
